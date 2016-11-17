87382774e43e9110ac007441fad47f62952ddb62
Alexandr Shushunov <ashushunov@mail.ru>
2016-11-17 17:16:43 +0300

    Add commands to deploy

--- deploy-image.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-docker build -t u2668/node-js-sceleton .
-
-docker push u2668/node-js-sceleton
-
-ssh root@46.101.204.43 docker stop back-end
-ssh root@46.101.204.43 docker rm back-end
-ssh root@46.101.204.43 docker pull u2668/node-js-sceleton
-ssh root@46.101.204.43 docker run -p 80:8080 -d --name back-end u2668/node-js-sceleton
---
+++ make-image-and-run.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+docker build -t u2668/node-js-sceleton .
+#docker push u2668/node-js-sceleton
+
+# local
+docker rm -f back-end
+docker run --name back-end --net go-to-canteen -d u2668/node-js-sceleton
+
+# remote
+#ssh root@46.101.204.43 docker rm -f back-end
+#ssh root@46.101.204.43 docker pull u2668/node-js-sceleton
+#ssh root@46.101.204.43 docker run --net go-to-canteen -d --name back-end u2668/node-js-sceleton
+
+# for debug
+#docker exec -it back-end /bin/bash
