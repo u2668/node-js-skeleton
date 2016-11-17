2c23f6dff7ad5824aea3297264062f3159960786
Alexandr Shushunov <ashushunov@mail.ru>
2016-11-17 17:15:53 +0300

    allow CORS

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ const PORT = 8080;
 
 // App
 const app = express();
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    next();
+});
+
 app.get('/', function (req, res) {
     res.send(`${logic.answer()}`);
 });
