a94edaa0d8e831550e7cea95a248a3c82610fa0a
Alexandr Shushunov <ashushunov@mail.ru>
2016-11-18 16:26:46 +0300

    Improve remote run  script

--- build-image.sh
+++ build-image.sh
@@ -2,14 +2,3 @@
 docker build -t u2668/node-js-sceleton .
 docker push u2668/node-js-sceleton
 
-# local
-#docker rm -f back-end
-#docker run --name back-end --net go-to-canteen -d u2668/node-js-sceleton
-
-# remote
-#ssh root@46.101.204.43 docker rm -f back-end
-#ssh root@46.101.204.43 docker pull u2668/node-js-sceleton
-#ssh root@46.101.204.43 docker run --net go-to-canteen -d --name back-end u2668/node-js-sceleton
-
-# for debug
-#docker exec -it back-end /bin/bash
--- run-remote.sh
+++ run-remote.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # remote
-ssh root@46.101.204.43 docker rm -f back-end
-ssh root@46.101.204.43 docker pull u2668/node-js-sceleton
-ssh root@46.101.204.43 docker run --net go-to-canteen -d --name back-end u2668/node-js-sceleton
+deployAddress="46.101.204.43"
+ssh root@${deployAddress} docker rm -f back-end
+ssh root@${deployAddress} docker pull u2668/node-js-sceleton
+ssh root@${deployAddress} docker run --net go-to-canteen -d --name back-end u2668/node-js-sceleton
